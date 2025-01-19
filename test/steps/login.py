2aa31b72bc142664879f22ccd684eabe5817b9fc
badrusalam11 <badrusalam760@gmail.com>
2025-01-19 18:10:24 +0700

    add capture in login

--- test/steps/login.py
+++ test/steps/login.py
@@ -5,10 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from test.test_objects.web_objects import WebObjects
 from test.global_variables.global_variables import GlobalVariables
+from test.utils.capture import Capture
 
 @given("I am on the login page")
 def step_open_login_page(context):
     context.driver.get(GlobalVariables.BASE_URL)
+    Capture.capture_screenshot(context, "login_page")
 
 @when("I log in with valid credentials")
 def step_login_with_valid_credentials(context):
@@ -16,8 +18,10 @@ def step_login_with_valid_credentials(context):
     context.driver.find_element(*WebObjects.LOGIN_LINK).click()
     context.driver.find_element(*WebObjects.USERNAME_FIELD).send_keys(GlobalVariables.USERNAME)
     context.driver.find_element(*WebObjects.PASSWORD_FIELD).send_keys(GlobalVariables.PASSWORD)
+    Capture.capture_screenshot(context, "fill_all_field")
     context.driver.find_element(*WebObjects.LOGIN_BUTTON).click()
 
 @then("I should see the appointment page")
 def step_verify_appointment_page(context):
     assert "Make Appointment" in context.driver.page_source
+    Capture.capture_screenshot(context, "make_appointment")
